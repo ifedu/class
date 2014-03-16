4a232f50b2ea4df5a3b0055c092e1dc59665d1a8
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2014-03-16 20:27:16 +0100

    1.4

--- Class.js
+++ Class.js
@@ -28,3 +28,39 @@ var ClassWithoutRequirejs = (function() {
 
     return me;
 }());
+
+define(function (require) {
+    'use strict';
+
+    var ClassWithRequirejs = (function() {
+        //var Import = require('./Class');
+        
+        //////me.publicStaticVar;
+
+        //////var privateStaticVar;
+
+        //////constructor
+        function me() {
+            //////this.publicVar;
+
+            //////var privateVar;
+
+            //////this.publicMethod = function() {};
+
+            //////function privateMethod() {}
+        }
+        //////me.prototype.publicMethod = function() {};
+
+        //////me.publicStaticMethod = function() {};
+
+        //////function privateStaticMethod() {}
+
+        //////INHERITANCE
+        //me.prototype             = new SuperClass();
+        //me.prototype.constructor = me;
+        //me.prototype.parent      = SuperClass.prototype;
+        //me.superClass            = SuperClass;
+        return me;
+    })();
+    return ClassWithRequirejs;
+}());
